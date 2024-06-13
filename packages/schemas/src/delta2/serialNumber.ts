c942e133193d3d6026200e908331f862ca02492a
Felix Hofmann <felix.hofmann@rustyy.de>
2024-06-13 21:06:46 +0200

    style: formatting

--- packages/schemas/src/delta2/serialNumber.ts
+++ packages/schemas/src/delta2/serialNumber.ts
@@ -6,11 +6,11 @@ import { z } from "zod";
  *********************************************/
 
 export const delta2SerialNumberSchema = z.custom<`R331${string}`>((val) => {
-    return typeof val === "string" ? /^R331/.test(val) : false;
+  return typeof val === "string" ? /^R331/.test(val) : false;
 });
 
 export type Delta2SerialNumber = z.infer<typeof delta2SerialNumberSchema>;
 
 export const isDelta2SerialNumber = (x: unknown): x is Delta2SerialNumber => {
-    return delta2SerialNumberSchema.safeParse(x).success;
+  return delta2SerialNumberSchema.safeParse(x).success;
 };
