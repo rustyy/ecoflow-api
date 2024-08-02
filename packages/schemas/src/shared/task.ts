ce291fe9580e052ee67bce7c9b3857c9b5c30dd2
Felix Hofmann <felix.hofmann@rustyy.de>
2024-08-02 22:54:45 +0200

    feat: pass through unknown properties to be still available to consumers

--- packages/schemas/src/shared/task.ts
+++ packages/schemas/src/shared/task.ts
@@ -4,25 +4,31 @@ import { z } from "zod";
  * Task schema seems to identical across all device types.
  */
 
-const timeSchema = z.object({
-  sec: z.number().int(),
-  week: z.number().int(),
-  min: z.number().int(),
-  hour: z.number().int(),
-  month: z.number().int(),
-  year: z.number().int(),
-  day: z.number().int(),
-});
+const timeSchema = z
+  .object({
+    sec: z.number().int(),
+    week: z.number().int(),
+    min: z.number().int(),
+    hour: z.number().int(),
+    month: z.number().int(),
+    year: z.number().int(),
+    day: z.number().int(),
+  })
+  .passthrough();
 
-export const taskSchema = z.object({
-  taskIndex: z.number().int(),
-  type: z.number().int(),
-  timeRange: z.object({
-    isConfig: z.boolean(),
-    isEnabled: z.boolean().optional(),
-    timeData: z.number().int(),
-    timeMode: z.number().int(),
-    startTime: timeSchema,
-    stopTime: timeSchema,
-  }),
-});
+export const taskSchema = z
+  .object({
+    taskIndex: z.number().int(),
+    type: z.number().int(),
+    timeRange: z
+      .object({
+        isConfig: z.boolean(),
+        isEnabled: z.boolean().optional(),
+        timeData: z.number().int(),
+        timeMode: z.number().int(),
+        startTime: timeSchema,
+        stopTime: timeSchema,
+      })
+      .passthrough(),
+  })
+  .passthrough();
