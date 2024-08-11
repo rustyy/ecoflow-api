bc121f35f4fce6dbdf89df2d0f4460ae6cb364c3
Felix Hofmann <felix.hofmann@rustyy.de>
2024-08-11 22:48:47 +0200

    feat(schemas:dp): Add deltaPro set command schemas
    Introduced schemas for various deltaPro set commands, including smart generator auto off, unit timeout, screen timeout, AC standby time, AC charging settings, PV charging type, and bypass AC auto start.

--- packages/schemas/src/deltaPro/setCommands.ts
+++ packages/schemas/src/deltaPro/setCommands.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 import { deltaProSerialNumberSchema } from "./serialNumber";
-import { zeroOrOne } from "../shared";
+import { integer, zeroOrOne } from "../shared";
 
 const defaultSchema = z.object({
   sn: deltaProSerialNumberSchema,
@@ -215,58 +215,180 @@ export type DeltaProSetSmartGeneratorAutoOn = z.infer<
   typeof deltaProSetSmartGeneratorAutoOnSchema
 >;
 
-// Setting the upper threshold percentage of smart generator auto off
-// {
-// "sn": "DCABZ*****",
-// "params": {
-// "cmdSet": 32, "id": 53, "closeOilSoc": 10
-// }
-// }
+/**
+ * Setting the upper threshold percentage of smart generator auto off
+ *
+ * @example
+ * ```json
+ * {
+ *    "sn": "DCABZ*****",
+ *    "params": {
+ *      "cmdSet": 32,
+ *      "id": 53,
+ *      "closeOilSoc": 10
+ *    }
+ * }
+ * ```
+ */
+export const deltaProSetSmartGeneratorAutoOffSchema = defaultSchema.extend({
+  params: defaultParams.extend({
+    id: z.literal(53),
+    closeOilSoc: z.number().int().min(0).max(100),
+  }),
+});
 
-// Setting the unit timeout
-// {
-// "sn": "DCABZ*****",
-// "params": {
-// "cmdSet": 32, "id": 33, "standByMode": 0
-// }
-// }
+export type DeltaProSetSmartGeneratorAutoOff = z.infer<
+  typeof deltaProSetSmartGeneratorAutoOffSchema
+>;
 
-// Setting the screen timeout
-// {
-// "sn": "DCABZ*****",
-// "params": {
-// "cmdSet": 32, "id": 39, "lcdTime": 60
-// }
-// }
+/**
+ * Setting the unit timeout
+ *
+ * @example
+ * ```json
+ * {
+ *    "sn": "DCABZ*****",
+ *    "params": {
+ *      "cmdSet": 32,
+ *      "id": 33,
+ *      "standByMode": 0
+ *    }
+ * }
+ * ```
+ */
+export const deltaProSetUnitTimeoutSchema = defaultSchema.extend({
+  params: defaultParams.extend({
+    id: z.literal(33),
+    standByMode: integer,
+  }),
+});
 
-// Setting the AC standby time
-// {
-// "sn": "DCABZ*****",
-// "params": {
-// "cmdSet": 32, "id": 153, "standByMins": 720
-// }
-// }
+export type DeltaProSetUnitTimeout = z.infer<
+  typeof deltaProSetUnitTimeoutSchema
+>;
 
-// AC charging settings
-// {
-// "sn": "DCABZ*****",
-// "params": {
-// "cmdSet": 32, "id": 69, "slowChgPower": 0
-// }
-// }
+/**
+ * Setting the screen timeout
+ *
+ * @example
+ * ```json
+ * {
+ *  "sn": "DCABZ*****",
+ *  "params": {
+ *    "cmdSet": 32,
+ *    "id": 39,
+ *    "lcdTime": 60
+ *  }
+ * }
+ * ```
+ */
+export const deltaProSetScreenTimeoutSchema = defaultSchema.extend({
+  params: defaultParams.extend({
+    id: z.literal(39),
+    lcdTime: integer,
+  }),
+});
 
-// PV charging type
-// {
-// "sn": "DCABZ*****",
-// "params": {
-// "cmdSet": 32, "id": 82, "chgType": 0
-// }
-// }
+export type DeltaProSetScreenTimeout = z.infer<
+  typeof deltaProSetScreenTimeoutSchema
+>;
 
-// Bypass AC auto start
-// {
-// "sn": "DCABZ*****",
-// "params": {
-// "cmdSet": 32, "id": 84, "enabled": 0
-// }
-// }
+/**
+ * Setting the AC standby time
+ *
+ * @example
+ * ```json
+ * {
+ *    "sn": "DCABZ*****",
+ *    "params": {
+ *      "cmdSet": 32,
+ *      "id": 153,
+ *      "standByMins": 720
+ *    }
+ * }
+ * ```
+ */
+export const deltaProSetACStandbyTimeSchema = defaultSchema.extend({
+  params: defaultParams.extend({
+    id: z.literal(153),
+    standByMins: integer,
+  }),
+});
+
+export type DeltaProSetACStandbyTime = z.infer<
+  typeof deltaProSetACStandbyTimeSchema
+>;
+
+/**
+ * AC charging settings
+ *
+ * @example
+ * ```json
+ * {
+ *    "sn": "DCABZ*****",
+ *    "params": {
+ *      "cmdSet": 32,
+ *      "id": 69,
+ *      "slowChgPower": 0
+ *    }
+ * }
+ * ```
+ */
+export const deltaProSetACChargingSchema = defaultSchema.extend({
+  params: defaultParams.extend({
+    id: z.literal(69),
+    slowChgPower: integer,
+  }),
+});
+
+export type DeltaProSetACCharging = z.infer<typeof deltaProSetACChargingSchema>;
+
+/**
+ * PV charging type
+ *
+ * @example
+ * ```json
+ * {
+ *    "sn": "DCABZ*****",
+ *    "params": {
+ *      "cmdSet": 32,
+ *      "id": 82,
+ *      "chgType": 0
+ *    }
+ * }
+ * ```
+ */
+export const deltaProSetPVChargingSchema = defaultSchema.extend({
+  params: defaultParams.extend({
+    id: z.literal(82),
+    chgType: integer,
+  }),
+});
+
+export type DeltaProSetPVCharging = z.infer<typeof deltaProSetPVChargingSchema>;
+
+/**
+ * Bypass AC auto start
+ *
+ * @example
+ * ```json
+ * {
+ *  "sn": "DCABZ*****",
+ *  "params": {
+ *    "cmdSet": 32,
+ *    "id": 84,
+ *    "enabled": 0
+ *  }
+ * }
+ * ```
+ */
+export const deltaProSetBypassACAutoStartSchema = defaultSchema.extend({
+  params: defaultParams.extend({
+    id: z.literal(84),
+    enabled: zeroOrOne,
+  }),
+});
+
+export type DeltaProSetBypassACAutoStart = z.infer<
+  typeof deltaProSetBypassACAutoStartSchema
+>;
